4fa7f7c1ab3a1b1da3fd6bfc0d2470d34c9cbe83
reghuramk <reghuramk04@gmail.com>
2025-06-10 21:25:30 +0100

    random code generator

--- fe/src/lib/utils.ts
+++ fe/src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function generateMeetCode() {
+  const chars = 'abcdefghijklmnopqrstuvwxyz';
+  const randomSegment = (length: number) =>
+    Array.from({ length }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
+
+  return `${randomSegment(3)}-${randomSegment(4)}-${randomSegment(3)}`;
+}
+
+console.log(generateMeetCode());
