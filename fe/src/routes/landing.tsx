c0db0c266b2d2589cbc171ff1220a650202e2890
reghuramk <reghuramk04@gmail.com>
2025-06-10 21:18:40 +0100

    added landing page buttons

---
+++ fe/src/routes/landing.tsx
@@ -0,0 +1,11 @@
+const LandingPage = () => {
+    return (
+        <div>
+            <button>Create link</button>
+            <input>Enter a code or link to join</input>
+        </div>
+    )
+}
+
+
+export default LandingPage
